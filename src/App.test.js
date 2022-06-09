127e1196f3b19e49bf4b4d0c8981501cb7fc4c7a
Radi Totev <raditotev@gmail.com>
2022-06-09 13:14:13 +0300

    Test App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('initial state', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByRole('heading', {
+    name: /find employees who worked on common projects/i,
+  });
+  const browseButton = screen.getByLabelText('Select file');
+  const table = screen.queryByRole('table');
+
+  expect(header).toBeInTheDocument();
+  expect(browseButton).toBeInTheDocument();
+  expect(table).not.toBeInTheDocument();
+});
+
+test('upload file', () => {
+  const file = new File(['employees'], 'employees.csv', { type: 'text/csv' });
+
+  render(<App />);
+
+  const input = screen.getByLabelText(/select file/i);
+  userEvent.upload(input, file);
+
+  expect(input.files[0]).toStrictEqual(file);
+  expect(input.files.item(0)).toStrictEqual(file);
+  expect(input.files).toHaveLength(1);
 });
